e1c67fa0b20633d988a12d5118accc6d26b1c64a
t-lieth <leithtorrens@gmail.com>
2021-08-05 14:22:49 +1200

    im confused need to work on this a bit more

--- chat.js
+++ chat.js
@@ -1,3 +1,6 @@
+// Collapsible
+console.log ("beans");
+
 var coll = document.getElementsByClassName("collapsible");
 
 for (let i = 0; i < coll.length; i++) {
@@ -14,3 +17,33 @@ for (let i = 0; i < coll.length; i++) {
 
     });
 }
+
+function getTime() {
+    let today = new Date();
+    hours = today.getHours();
+    minutes = today.getMinutes();
+
+    if (hours < 10) {
+        hours = "0" + hours;
+    }
+
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+
+    let time = hours + ":" + minutes;
+    return time;
+}
+
+// Gets the first message
+function firstBotMessage() {
+    let firstMessage = "How's it going?"
+    document.getElementById("botStarterMessage").innerHTML = '<p class="botText"><span>' + firstMessage + '</span></p>';
+
+    let time = getTime();
+
+    $("#chat-timestamp").append(time);
+    document.getElementById("userInput").scrollIntoView(false);
+}
+
+firstBotMessage();
